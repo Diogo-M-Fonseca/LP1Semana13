1024eb5a1dfd6967e13c6d9dfc1ce55e3b8e8327
Diogo-M-Fonseca <Diogofonseca.gamedesigner@gmail.com>
2025-05-30 00:22:24 +0100

    IsPalindrome method created

---
+++ Palindrome/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+        private bool IsPalindrome(string s)
+            {
+                if (s == null)
+                {
+                    throw new ArgumentNullException(nameof(s));
+                }
+
+                if (s.Length < 2)
+                {
+                    return true;
+                }
+            
+            }
+    }
+}
